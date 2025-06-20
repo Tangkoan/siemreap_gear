34cb1253381e0d7eda10d66a4f5a33680125413f
Tangkoan <kuytangkoan@gmail.com>
2025-06-20 13:57:57 +0700

    admin setup with role part4

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -186,14 +186,15 @@ class AdminController extends Controller
         <td class="p-4 py-5">' . ($item->email ?? 'null') . '</td>
         <td class="p-4 py-5">' . ($item->phone ?? 'null') . '</td> 
         
-       <td class="p-4 py-5 text-sm text-black">' . ($item->roles/name) . '</td>
+       <td class="p-4 py-5 text-sm text-black">' . implode(', ', $item->getRoleNames()->toArray()) . '</td>
+
 
         
         <td class="px-4 py-4 text-sm whitespace-nowrap">
             <div class="flex items-center gap-x-6">
             
                 <button class="icon-edit text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
-                    <a href="' . route('edit.product', $item->id) . '" >
+                    <a href="' . route('edit.admin', $item->id) . '" >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                         </svg>
@@ -201,7 +202,7 @@ class AdminController extends Controller
                 </button>
     
                         <button type="button" class="icon-delete text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
-                                    <a href="' . route('delete.product', parameters: $item->id) . '" id="delete">
+                                    <a href="' . route('delete.admin', parameters: $item->id) . '" id="delete">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                     </svg>
@@ -272,5 +273,62 @@ class AdminController extends Controller
     return redirect()->route('all.admin')->with($notification);
 }
 
+
+    public function EditAdmin($id){
+
+        $roles = Role::all();
+        $adminuser = User::findOrFail($id);
+        return view('admin.admin.edit_admin',compact('roles','adminuser'));
+
+    }// End Method 
+
+
+    
+
+    public function UpdateAdmin(Request $request)
+    {
+        $admin_id = $request->id;
+    
+        $user = User::findOrFail($admin_id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone; 
+        $user->save();
+    
+        // Remove existing roles
+        $user->roles()->detach();
+    
+        // Assign new role if exists
+        if ($request->roles) {
+            $role = Role::findById($request->roles); // ដាក់ role ID នៅទីនេះ
+            $user->assignRole($role->name); // បម្លែងទៅជា name មុន
+        }
+    
+        $notification = array(
+            'message' => 'Admin User Updated Successfully',
+            'alert-type' => 'success'
+        );
+    
+        return redirect()->route('all.admin')->with($notification);  
+    }
+    
+
+
+
+    public function DeleteAdmin($id){
+
+        $user = User::findOrFail($id);
+        if (!is_null($user)) {
+            $user->delete();
+        }
+
+        $notification = array(
+            'message' => 'Admin User Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification); 
+
+    }// End Method 
 
 }
