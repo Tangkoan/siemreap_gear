552dc7d0701fa1c865d7ee192fbbbe6721aa75d2
Tangkoan <kuytangkoan@gmail.com>
2025-08-12 16:57:27 +0700

    update some code for auto backup_google

--- routes/console.php
+++ routes/console.php
@@ -8,28 +8,34 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+// ============ Test ពីរនាទីម្ដង ============== //
+    // Daily DB only @ 03:11
+    // រត់ Backup database ម្ដងក្នុងមួយថ្ងៃ នៅម៉ោង 3:30 PM (15:30)
+        Schedule::command('backup:run --only-db --only-to-disk=google')
+             ->dailyAt('17:20')
+             ->withoutOverlapping();
 
-// ---------------------- Test ពីរនាទីម្ដង ------------------------ //
-
-// ✅ សម្រាប់តេស្តបណ្ដោះអាសន្ន
-// Schedule::command('backup:run --only-db --only-to-disk=google')->everyTwoMinutes()->withoutOverlapping();
-Schedule::command('backup:run --only-db --only-to-disk=google')->everyTwoMinutes();
-
-// Daily DB only @ 01:00
-Schedule::command('backup:run --only-db --only-to-disk=google')->dailyAt('01:00')->withoutOverlapping();
-
-
-// Weekly full @ Sun 02:00 → Google only
-Schedule::command('backup:run --only-to-disk=google')
-    ->weeklyOn(0, '02:00')->withoutOverlapping();
-
-// Cleanup & Monitor → Google only
-// Schedule::command('backup:clean --only-to-disk=google')->dailyAt('03:00');
-// Schedule::command('backup:monitor --only-to-disk=google')->dailyAt('04:00');
+    // Clean up old backups ម្ដងក្នុងមួយថ្ងៃ បន្ទាប់ពី backup បានជោគជ័យ
+        Schedule::command('backup:clean')
+            ->dailyAt('17:25') // កំណត់ cleanup (5 នាទីបន្ទាប់ពី backup)
+            ->withoutOverlapping();
 
 
+    // Weekly full @ Sun 02:00 → Google only
+        Schedule::command('backup:run --only-to-disk=google')
+            ->weeklyOn(0, '17:30')
+            ->withoutOverlapping();
+
+    // Weekly full @ Sun 02:00 → Google only
+        Schedule::command('backup:clean')
+            ->weeklyOn(0, '17:30')
+            ->withoutOverlapping();
 
 
-// ✅ Cleanup សម្រាប់តេស្ត
-// Schedule::command('backup:clean')->everyTwoMinutes()->withoutOverlapping();
-Schedule::command('backup:clean')->everyTwoMinutes();
+
+
+// ================== Code សម្រាប់ Test 2នាទីម្ដង ===================
+    // // ✅ សម្រាប់តេស្តបណ្ដោះអាសន្ន
+    // Schedule::command('backup:run --only-db --only-to-disk=google')->everyTwoMinutes();
+    // // ✅ Cleanup សម្រាប់តេស្ត
+    // Schedule::command('backup:clean')->everyTwoMinutes()->withoutOverlapping();
