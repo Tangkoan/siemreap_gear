5fc2c2046da9774b8d86135054f16a02a1d37e2c
Kuy Tangkoan <kuytangkoan@gmail.com>
2025-11-07 15:02:18 +0700

    update

--- resources/views/admin/category/all_category.blade.php
+++ resources/views/admin/category/all_category.blade.php
@@ -53,9 +53,9 @@
                         <div class="ml-3">
                             <div class="w-full max-w-sm min-w-[200px] relative">
                                 <div class="relative">
-                                    <input class=" card-dynamic-bg text-defalut   w-full pr-11 h-10 pl-3 py-2  placeholder:text-slate-400 text-slate-700 text-sm border-slate-300  rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md" placeholder="{{ __('messages.search_for_category') }}" id="search" name="search" type="text" />
+                                    <input class=" card-dynamic-bg text-defalut   w-full pr-11 h-10 pl-3 py-2  placeholder:text-slate-400  text-sm border-primary  rounded transition duration-200 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md" placeholder="{{ __('messages.search_for_category') }}" id="search" name="search" type="text" />
                                     <button class="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center   rounded" type="button">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-8 h-8 text-slate-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-8 h-8 text-primary">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                         </svg>
                                     </button>
--- resources/views/admin/condition/all_condition.blade.php
+++ resources/views/admin/condition/all_condition.blade.php
@@ -52,7 +52,7 @@
                                 <div class="relative">
                                     <input class="card-dynamic-bg text-defalut w-full pr-11 h-10 pl-3 py-2 placeholder:text-slate-400  text-sm sm:text-sm border border-primary rounded transition duration-200 ease focus:outline-none  shadow-sm focus:shadow-md" placeholder="{{ __('messages.search') }}" id="search" name="search" type="text" />
                                     <button class="absolute h-8 w-8 right-1 top-1 my-auto px-2 flex items-center rounded" type="button" aria-label="search">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-6 h-6 sm:w-8 sm:h-8 text-defalut">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-6 h-6 sm:w-8 sm:h-8 text-primary">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                         </svg>
                                     </button>
